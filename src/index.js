776fc0055bcee84748bc894bd18d0c051cc687e3
caominhbao444 <minhbao11b3@gmail.com>
2023-09-19 16:55:49 +0700

    static views

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+  })
+); // Corrected function name
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/news", function (req, res) {
+  res.render("new");
+});
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
